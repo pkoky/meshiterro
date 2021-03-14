ca75d327e5d5e8eece54d15e83aa53a5a23d00c4
pkoky <sscsk2224@gmail.com>
2021-03-14 06:17:14 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
     
     resources :post_comments, only: [:create, :destroy]
   end 
+  resources :usrs, only: [:show]
   
   
 end
